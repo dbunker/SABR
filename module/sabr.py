f5d0f3a14612dd3dd09b1c0a6e432a288c89e2b6
vagrant <vagrant@precise64.(none)>
2012-12-04 20:32:02 +0000

    module

--- module/sabr.py
+++ module/sabr.py
@@ -2,22 +2,25 @@
 class SabrObj:
 	
 	def __init__(self):
-    	self.sym = []
-
-	def setSym(self):
+		self.sym = []
+		self.board = []
+		self.req = []
+		self.desobj = []
+    	
+	def setSym(self,arr):
+		self.sym = arr;
 		
+	def setBoard(self,arr):
+		self.board = arr;
 		
-	def setBoard(self):
+	def addReq(self,arr):
+		self.req = arr;
 		
+	def addAllDif(self,arr):
+		self.sym = arr;
 		
-	def setReq(self):
-		
-		
-	def setAllDif(self):
-		
-		
-	def setDesObj(self):
-		
+	def addDesObj(self,arr):
+		self.sym = arr;
 		
 	def process(self):
-		
+		print 'Start'
--- module/sudoku-test/sudoku.py
+++ module/sudoku-test/sudoku.py
@@ -1,3 +1,35 @@
 # top95.txt from http://norvig.com/sudoku.html
 
+import sys
+sys.path.append("..")
 
+import sabr
+
+# sym
+['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+# req
+letterArr = 'abcdefghi'
+
+boardArr = [['.']*9 for x in xrange(9)]
+for y in range(0,9):
+	for x in range(0,9):
+		boardArr[y][x] = letterArr[y] + str(x)
+
+# desobj
+
+
+
+# for each new line in file create new board
+fi = open('top95.txt')
+for line in fi:
+	arr = list(line)
+	print arr
+
+# board
+for y in range(0,9):
+	for x in range(0,9):
+		""
+
+print 'hi'
+# process
