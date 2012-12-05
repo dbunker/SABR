e7a331d8add2a2336ed18454ff7c00c26cf4490a
vagrant <vagrant@precise64.(none)>
2012-12-05 04:50:21 +0000

    module

--- module/sabr.py
+++ module/sabr.py
@@ -1,11 +1,13 @@
 
+import os
+
 class SabrObj:
 	
 	def __init__(self):
 		self.sym = []
 		self.board = []
-		self.req = []
-		self.desobj = []
+		self.reqList = []
+		self.desObjList = []
     	
 	def setSym(self,arr):
 		self.sym = arr;
@@ -13,14 +15,92 @@ class SabrObj:
 	def setBoard(self,arr):
 		self.board = arr;
 		
-	def addReq(self,arr):
-		self.req = arr;
+	def addReq(self,name,arr):
+		self.reqList.append((name,arr))
+	
+	def addAllDif(self,name,num):
 		
-	def addAllDif(self,arr):
-		self.sym = arr;
+		for y in range(0,num):
+			req = []
+			for x in range(0,num):
+				val = '!v'
+				if x == y:
+					val = ' v'
+				req.append(val)
+			
+			self.addReq(name,req)
+	
+	def addDesObj(self,name,arr):
+		self.desObjList.append((name,arr))
 		
-	def addDesObj(self,arr):
-		self.sym = arr;
+	def addReqSpace(self):
+		self.reqList.append((None,None))
+
+	def addDesObjSpace(self):
+		self.desObjList.append((None,None))
+	
+	def outElem(self,arr):
 		
-	def process(self):
-		print 'Start'
+		out = '{'
+		if isinstance(arr[0],list):
+			out += '\n\t'
+			for line in arr:
+				for elem in line:
+					out += elem + ' '
+				out = out[:-1] + ';\n\t'
+			out = out[:-1]
+			
+		else:
+			for elem in arr:
+				out += ' ' + elem 
+			out += ' '
+		
+		out += '}\n'	
+		return out
+	
+	def outArray(self,type,arr):
+		out = ''
+		for (name,req) in arr:
+			if name == None:
+				out += '\n'
+			else:
+				out += type + ' ' + name
+				out += self.outElem(req)
+		return out
+	
+	def toString(self):
+		
+		# sym
+		out = 'Sym' + self.outElem(self.sym) + '\n'
+		
+		# board
+		out += 'Board' + self.outElem(self.board) + '\n'
+		
+		# req
+		out += self.outArray('Req',self.reqList) + '\n'
+		
+		# desobj
+		out += self.outArray('DesObj',self.desObjList) + '\n'
+		
+		return out
+	
+	# source.tb
+	def process(self,sabrPath):
+	
+		out = self.toString()
+		sourceFile = open('source.tb','w')
+		sourceFile.write(out)
+		sourceFile.close()
+		
+		os.system(sabrPath + ' 1 source.tb > stats.txt')
+		
+		resultFile = open('result.txt','r')
+		res = resultFile.read()
+		resultFile.close()
+		
+		statsFile = open('stats.txt','r')
+		stat = statsFile.read()
+		statsFile.close()
+		
+		return (res,stat)
+		
--- module/sudoku-test/sudoku.py
+++ module/sudoku-test/sudoku.py
@@ -2,34 +2,79 @@
 
 import sys
 sys.path.append("..")
-
 import sabr
 
-# sym
-['1', '2', '3', '4', '5', '6', '7', '8', '9']
+def makeConstraints(boardSet):
 
-# req
-letterArr = 'abcdefghi'
+	letterArr = 'abcdefghi'
+	sabrObj = sabr.SabrObj()
+	
+	# sym
+	sabrObj.setSym(['1','2','3','4','5','6','7','8','9'])
+	
+	# board
+	boardArr = [['.']*9 for x in xrange(9)]
+	for y in range(0,9):
+		for x in range(0,9):
+			boardArr[y][x] = letterArr[y] + str(x)
+			
+	sabrObj.setBoard(boardArr)
+	
+	# req board
+	boardArr = [['.']*9 for x in xrange(9)]
+	for y in range(0,9):
+		for x in range(0,9):
+			val = boardSet[y*9+x]
+			if val == '.':
+				val = '?'
+			boardArr[y][x] = val
+			
+	sabrObj.addReq('Board',boardArr)
+	sabrObj.addReqSpace()
+	
+	# all different
+	sabrObj.addAllDif('RowColBlock',9)
+	
+	# desobj row
+	for y in range(0,9):
+		desObjRow = []
+		for x in range(0,9):
+			desObjRow.append(letterArr[y] + str(x))
+		sabrObj.addDesObj('RowColBlock',desObjRow)
+		
+	sabrObj.addDesObjSpace()
+	
+	# desobj column
+	for y in range(0,9):
+		desObjCol = []
+		for x in range(0,9):
+			desObjCol.append(letterArr[x] + str(y))
+		sabrObj.addDesObj('RowColBlock',desObjCol)
+		
+	sabrObj.addDesObjSpace()
+	
+	# desobj block
+	for blockY in range(0,3):
+		for blockX in range(0,3):
+		
+			desObjBlock = []
+			for y in range(0,3):
+				for x in range(0,3):
+					val = letterArr[blockY*3+y] + str(blockX*3+x)
+					desObjBlock.append(val)
+					
+			sabrObj.addDesObj('RowColBlock',desObjBlock)
+	
+	return sabrObj
 
-boardArr = [['.']*9 for x in xrange(9)]
-for y in range(0,9):
-	for x in range(0,9):
-		boardArr[y][x] = letterArr[y] + str(x)
-
-# desobj
-
-
-
-# for each new line in file create new board
-fi = open('top95.txt')
-for line in fi:
+# for each new line in file create new board req
+probs = open('top95.txt','r')
+for line in probs:
 	arr = list(line)
-	print arr
+	
+	sabrObj = makeConstraints(arr)
+	
+	# process
+	(out,stats) = sabrObj.process('../../sabr')
+	print line,out,stats
 
-# board
-for y in range(0,9):
-	for x in range(0,9):
-		""
-
-print 'hi'
-# process
