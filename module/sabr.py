1a42afe0f2d1a2fec62e91cad92409b962c85ae0
vagrant <vagrant@precise64.(none)>
2012-12-21 15:20:46 +0000

    module

--- module/sabr.py
+++ module/sabr.py
@@ -154,124 +154,171 @@ class SabrObj:
 					transDesObjList.append((constTrans,constDesObj))
 	
 		for x in transDesObjList:
-		
-			((_,nameTransX,objX,_,startArrX,endArrX),(_,nameDesObjX,_,_,desObjX,_)) = x
 			
 			for y in transDesObjList:
-				
-				((_,nameTransY,objY,_,startArrY,endArrY),(_,nameDesObjY,_,_,desObjY,_)) = y
-				
-				def embedList(arr):
-					arr = copy.deepcopy(arr)
-					if isinstance(arr[0],list):
-						return arr
-					return [arr]
-				
-				def makeList(arr1,arr2):
-				
-					arr1 = embedList(arr1)
-					arr2 = embedList(arr2)
-					return arr1 + arr2
-					
-				firstTransStart = embedList(startArrX)
-				firstTransEnd = embedList(endArrX)
-				firstTransDesObj = embedList(desObjX)
-				
-				secondTransStart = embedList(startArrY)
-				secondTransEnd = embedList(endArrY)
-				secondTransDesObj = embedList(desObjY)
-				
-				# becomes start and end of mainTrans
-				mainTransPartFirst = embedList(startArrX)
-				mainTransPartSecond = embedList(startArrY)
-				
-				####################
-				
-				# v1 v2;
-				# w1 w2;
-	
-				# T1) v1 v2 => v2 v1
-				# T2) w1 w2 => w2 w1
-
-				# a b;
-				# b c;
-				
-				firstTransStart = [['v1','v2']]
-				firstTransEnd = [['v2', 'v1']]
-				firstTransDesObj = [['a', 'b']]
-				
-				secondTransStart = [['w1', 'w2']]
-				secondTransEnd = [['w2', 'w1']]
-				secondTransDesObj = [['b', 'c']]
-				
-				# becomes start and end of mainTrans
-				mainTransPartFirst = [['v1', 'v2']]
-				mainTransPartSecond = [['w1', 'w2']]
-				
-				height = len(firstTransDesObj)
-				width = len(firstTransDesObj[0])
-				
-				allCells = [(y, x) for x in range(width) for y in range(height)]
-				
-				# remove all duplicate cells from mainTransPartSecond based on first cells
-				cellsPresent = {}
-				for (a,b) in allCells:
-					elem = firstTransDesObj[a][b]
-					cellsPresent[elem] = firstTransStart[a][b]
-				
-				for (a,b) in allCells:
-					elem = secondTransDesObj[a][b]
-					if elem in cellsPresent:
-						secondTransStart[a][b] = cellsPresent[elem]
-				
-				startTransPartFirst = embedList(mainTransPartFirst)
-				startTransPartSecond = embedList(mainTransPartSecond)
-				
-				# perform 1st trans
-				
-				# find all
-				
-				# use TransSim, do not have DesObj with no common values form a new trans
-				# remove redundant or useless transitions
-
-				# T1) v1 v2 => v2 v1
-				# a b;
-
-				# T2) w1 w2 => w2 w1
-				# b c;
-
-				# TM) 
-
-				# b0: [v2]
-				# b1: [v1]		[w1]
-				# b2: 			[w2]
-
-				# a			b		c
-				# v1-w1		v2		w2		=>
-				# v2		v1-w1	w2		=>
-				# v2		w2		v1-w1
-
-				# match T1_End to T2_Start
- 
-				
-				# change secondTrans to match new board
-				
-				# perform 2nd trans
-				
-				# create final result to add
-				
-				# remove redundancy
+		
+				self.unrollTrans(x,y)
 				
 				exit()
+		
+	def unrollTrans(self,trans1,trans2):
+	
+		((_,nameTransX,objX,_,startArrX,endArrX),(_,nameDesObjX,_,_,desObjX,_)) = trans1
+		((_,nameTransY,objY,_,startArrY,endArrY),(_,nameDesObjY,_,_,desObjY,_)) = trans2
+	
+		####################
+		
+		# T1) v1 v2 => v2 v1
+		# a b;
+
+		# T2) w1 w2 => w2 w1
+		# b c;
+		
+		# T1) a0:(v1) b0:(v2) => a1:(v2) b1:(v1)
+
+		# T2) b1:(w1) c1:(w2) => b2:(w2) c2:(w1)
+		
+		t1_StartS = {0:'v1', 1:'v2'}
+		t1_EndS = {0:'v2', 1:'v1'}
+		t1_DesObjS = {0:'a', 1:'b'}
+		
+		t2_StartS = {0:'w1', 1:'w2'}
+		t2_EndS = {0:'w2', 1:'w1'}
+		t2_DesObjS = {0:'b', 1:'c'}
+		
+		####################
+		
+		t1_Start = {}
+		t1_End = {}
+		
+		t2_Start = {}
+		t2_End = {}
+		
+		# set T1-0
+		for k,v in t1_DesObjS.items():
+			t1_Start[k] = ((v,0),t1_StartS[k])
+		
+		# set T1-1
+		for k,v in t1_DesObjS.items():
+			t1_End[k] = ((v,1),t1_EndS[k])
+		
+		# set T2-1
+		for k,v in t2_DesObjS.items():
+			t2_Start[k] = ((v,1),t2_StartS[k])
+		
+		# set T2-2
+		for k,v in t2_DesObjS.items():
+			t2_End[k] = ((v,2),t2_EndS[k])
+		
+		allT1 = copy.deepcopy([t1_Start, t1_End])
+		allT2 = copy.deepcopy([t2_Start, t2_End])
+		
+		####################
+		
+		def replace(all,x,y):
+			for li in all:
+				for k,(cell,var) in li.items():
+					if var == x:
+						li[k] = (cell,y)
+			
+		# match T1_End to T2_Start
+		# note, symbol can't be bound to var
+		
+		numCellOverlaps = 0
+		
+		for k1,(cell1,var1) in t1_End.items():
 				
-				# names
-				newObjId = objX + '-' + objY
-				newTransId = nameTransX + '-' + nameTransY
-				newDesObjId = nameDesObjX + '-' + nameDesObjY
+			for k2,(cell2,var2) in t2_Start.items():
 				
-				self.addTrans(newTransId,newObjId,newStartArr,newEndArr)
-				self.addDesObj(newDesObjId,newObjId,newDesObj)
-				self.addSpace()
+				if cell1 == cell2:
+					numCellOverlaps += 1
+					
+					# confirm these are compatible
+					if var1 in self.sym:
+					
+						if var2 in self.sym:
+						
+							if var1 != var2:
+								print 'Cannot Unroll Due To Conflict'
+								return
+							else:
+								continue
+					
+					# replace all var1 with var1-var2
+					replace(allT1,var1,var1+'-'+var2)
+					
+					# replace all var2 with var1-var2
+					replace(allT2,var2,var1+'-'+var2)
+		
+		if numCellOverlaps == 0:
+			print 'Use TransSim Instead of Unroll'
+			return
+		
+		####################
+		# apply first based on what appears first
+		# apply last based on what appears last
+		
+		t_Start = []
+		t_End = []
+		des = []
+		
+		# go through desobj
+		firstDict = {}
+		for k,v in t1_DesObjS.items():
+			((name,stage),var) = allT1[0][k]
+			t_Start.append(var)
+			des.append(name)
+			firstDict[name] = True
+		
+		for k,v in t2_DesObjS.items():
+			((name,stage),var) = allT2[0][k]
+			if not name in firstDict:
+				t_Start.append(var)
+				des.append(name)
+		
+		searchDes = copy.deepcopy(des)
+		for desName in searchDes:
+			
+			def findIn(allList,found,desName):
+			
+				for k,((name,stage),var) in allList.items():
+					if name == desName:
+						if found == False:
+							found = ((name,stage),var)
+							t_End.append(var)
+						elif found[0][1] == stage:
+						
+							# must add for additional constraint
+							des.append(name)
+							t_Start.append('?')
+							t_End.append(var)
+							
+				return found
+						
+			found = False
+			
+			# try to find in last
+			found = findIn(allT2[1],found,desName)
+			
+			# find in second to last
+			found = findIn(allT1[1],found,desName)
+			
+		print t_Start
+		print t_End
+		print des
+		
+		####################
+		
+		exit()
+		
+		# names
+		newObjId = objX + '-' + objY
+		newTransId = nameTransX + '-' + nameTransY
+		newDesObjId = nameDesObjX + '-' + nameDesObjY
+		
+		self.addTrans(newTransId,newObjId,newStartArr,newEndArr)
+		self.addDesObj(newDesObjId,newObjId,newDesObj)
+		self.addSpace()
 				
 	def source(self,fileName):
 	
